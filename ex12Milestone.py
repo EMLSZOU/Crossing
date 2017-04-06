4f970a450f50369c7b8b971b53b49321e2511cb0
aChing <aChing@live.cn>
2017-04-07 00:19:40 +0800

    Clean

--- ex12Milestone.py
+++
@@ -1,7 +0,0 @@
-#我的第一个Python程序，实现了控制
-print ("Am I a cute boy? Answer yes or no.")
-answer = input()
-while answer == "no":
-    print ("Your answer was wrong. Answer again, please.")
-    answer = input()    
-print ("You are smart, boy.")
